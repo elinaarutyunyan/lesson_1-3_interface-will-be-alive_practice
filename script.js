223f57f89be19a826524977c6126bf529edeca49
david-arutyunyan <daarutyunyan@edu.hse.ru>
2026-09-22 20:27:46 +0300

    task-3

--- script.js
+++ script.js
@@ -13,9 +13,12 @@ const eventName = "Город в деталях";
 let clickCount = 0;
 
 firstCard.addEventListener("click", () => {
-  labStatus.textContent = "Открыта первая карточка!";
-    clickCount = clickCount + 1;
-  labStatus.textContent = `${eventName}: нажатий ${clickCount}`;
+  clickCount = clickCount + 1;
+  if (clickCount === 1) {
+    labStatus.textContent = `Первое открытие: ${eventName}`;
+  } else {
+    labStatus.textContent = `Повторное открытие: ${eventName}`;
+  }
 });
 
 // 02–03. Вы будете постепенно менять обработчик выше: добавите
